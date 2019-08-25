fecd736901cf97f97bd1092c6c5d8fee09faf2b2
Manish Bansal <manishbansal8843@gmail.com>
2019-08-26 00:03:24 +0530

    build config edited to check cluster existence

--- gke-setup/enable-google-services.sh
+++ gke-setup/enable-google-services.sh
@@ -10,5 +10,20 @@ project=$(gcloud projects list --format="value(projectId)")
 echo "Available google services for the project $project"
 gcloud services list
 echo "===================================="
+echo "Checking if gke cluster existed"
+echo "===================================="
+does_cluster_exists=false
+for clusters in  $(gcloud container clusters list --format="value(name)")
+do
+  echo "clusters:  $clusters"  
+  if [ $clusters == $CLOUDSDK_CONTAINER_CLUSTER ]
+    does_cluster_exists=true
+    echo "$CLOUDSDK_CONTAINER_CLUSTER already exists. hence, will not try creating it again."
+  fi
+done
+if [ $does_cluster_exists == false]
+echo "===================================="
 echo "Creating gke cluster"
+echo "===================================="
 gcloud container clusters create $CLOUDSDK_CONTAINER_CLUSTER
+fi
