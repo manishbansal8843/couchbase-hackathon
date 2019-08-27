ee56787bb4257b00ad2d79bf2cf1a78abc191710
Manish Bansal <manishbansal8843@gmail.com>
2019-08-27 15:19:38 +0530

    Adding couchbase operator sample files

---
+++ gke-setup/clean-up-gke.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Cleaning up GKE"
+echo "==============================="
+gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
+echo "===================================="
+echo "Checking if gke cluster existed"
+echo "===================================="
+for clusters in  $(gcloud container clusters list --format="value(name)")
+do
+  echo "Starting deletion of cluster:  $clusters"   
+  gcloud container clusters delete $clusters --async
+  echo "$clusters deleted successfully."
+done
+echo "GKE clean-up completed successfully."
